9494225ff567066eeb9c005fe69b531122e416b0
Kashyap <kashyap.ekbote@gmail.com>
2021-03-18 16:15:52 +0530

    Tcp Session will handle one client
    Every new client connted then
    new tcp session will be created
    if client disconnect then tcp session will be close

---
+++ tcp-session.cxx
@@ -0,0 +1,52 @@
+/* Copyright (C) 2008-2021 HiPro IT Solutions Private Limited,
+ * Chennai. All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms described in the LICENSE file which accompanies
+ * this distribution. If the LICENSE file was not attached to this
+ * distribution or for further clarifications, please contact
+ * legal@hipro.co.in. */
+
+#include "tcp-session.h"
+
+void
+TcpSession::start()
+{
+  m_socket.async_read_some(
+    boost::asio::buffer(m_data, MAX_LENGTH),
+    boost::bind(
+      &TcpSession::handle_read, this, boost::asio::placeholders::error,
+      boost::asio::placeholders::bytes_transferred));
+}
+
+void
+TcpSession::handle_read(
+  const boost::system::error_code& error, size_t bytes_transferred)
+{
+  if (!error) {
+    boost::asio::async_write(
+      m_socket, boost::asio::buffer(m_data, bytes_transferred),
+      boost::bind(
+        &TcpSession::handle_write, this, boost::asio::placeholders::error));
+    std::cout << "Pong Send" << std::endl;
+  }
+}
+
+void
+TcpSession::handle_write(const boost::system::error_code& error)
+{
+  if (!error) {
+    m_socket.async_read_some(
+      boost::asio::buffer(m_data, MAX_LENGTH),
+      std::bind(
+        &TcpSession::handle_read, this, std::placeholders::_1,
+        std::placeholders::_2));
+    std::cout << "Got Ping" << std::endl;
+  }
+}
+
+/*
+  Local Variables:
+  mode: c++
+  End:
+*/
---
+++ tcp-session.h
@@ -0,0 +1,48 @@
+/* Copyright (C) 2008-2021 HiPro IT Solutions Private Limited,
+ * Chennai. All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms described in the LICENSE file which accompanies
+ * this distribution. If the LICENSE file was not attached to this
+ * distribution or for further clarifications, please contact
+ * legal@hipro.co.in. */
+
+#ifndef HIPRO__bcb24458_87a7_11eb_8e3f_b9b5ee5d4b95
+#define HIPRO__bcb24458_87a7_11eb_8e3f_b9b5ee5d4b95
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <cstdlib>
+#include <iostream>
+
+class TcpSession
+{
+public:
+  TcpSession(boost::asio::io_service& io_service) : m_socket(io_service) {}
+  boost::asio::ip::tcp::socket& socket()
+  {
+    return m_socket;
+  }
+  void start();
+
+private:
+  void handle_read(
+    const boost::system::error_code& error, size_t bytes_transferred);
+  void handle_write(const boost::system::error_code& error);
+
+private:
+  boost::asio::ip::tcp::socket m_socket;
+  enum
+  {
+    MAX_LENGTH = 1024
+  };
+  char m_data[MAX_LENGTH];
+};
+
+#endif // HIPRO__bcb24458_87a7_11eb_8e3f_b9b5ee5d4b95
+
+/*
+  Local Variables:
+  mode: c++
+  End:
+*/
