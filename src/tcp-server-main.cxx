38962c02af4f140c205a8dff531fb746fd79a865
Kashyap <kashyap.ekbote@gmail.com>
2021-03-26 19:34:23 +0530

    Early exit added on error checking

--- src/tcp-server-main.cxx
+++ src/tcp-server-main.cxx
@@ -9,43 +9,50 @@
 
 #include <cstdlib>
 #include <cstdint>
-#include <arpa/inet.h>
 
 #include "tcp-server-app.h"
 
 int
 main(int argc, char* argv[])
 {
-  try {
-    if (argc != 4) {
-      std::cerr << "Usage error: " << argv[0]
-                << " <broadcast_port> <tcp_port> <tcp_ip>" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-    int tcp_port_int = std::stoi(argv[2]);
-    int broadcast_port_int = std::stoi(argv[1]);
-    std::string tcp_ip = argv[3];
-
-    if (
-      (tcp_port_int <= static_cast<int>(UINT16_MAX) && tcp_port_int >= 0) &&
-      (broadcast_port_int <= static_cast<int>(UINT16_MAX) &&
-       broadcast_port_int >= 0)) {
-
-      uint16_t tcp_port_short = static_cast<uint16_t>(tcp_port_int);
-      uint16_t broadcast_port = static_cast<uint16_t>(broadcast_port_int);
-
-      TcpServerApp tcp_server_app(tcp_port_short);
-      tcp_server_app.run(broadcast_port, tcp_ip);
-
-    } else {
-      std::runtime_error("Port must be less than 65535");
-    }
-
-  } catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
+  if (argc != 4) {
+    std::cerr << "Usage error: " << argv[0]
+              << " <broadcast_port> <tcp_port> <tcp_ip>" << std::endl;
+    return EXIT_FAILURE;
   }
 
-  return 0;
+  int broadcast_port_int;
+  int tcp_port_int;
+  std::string tcp_ip;
+
+  try {
+    broadcast_port_int = std::stoi(argv[1]);
+    tcp_port_int = std::stoi(argv[2]);
+    tcp_ip = argv[3];
+  } catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (tcp_port_int >= static_cast<int>(UINT16_MAX) && tcp_port_int <= 0) {
+    std::cerr << "Tcp Port must be less than 65535" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (
+    broadcast_port_int >= static_cast<int>(UINT16_MAX) &&
+    broadcast_port_int <= 0) {
+    std::cerr << "Broadcast Port must be less than 65535" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  uint16_t tcp_port_short = static_cast<uint16_t>(tcp_port_int);
+  uint16_t broadcast_port = static_cast<uint16_t>(broadcast_port_int);
+
+  TcpServerApp tcp_server_app(tcp_port_short);
+  tcp_server_app.run(broadcast_port, tcp_ip);
+
+  return EXIT_SUCCESS;
 }
 
 /*
